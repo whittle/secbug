0777a4bd28e4b669a55cbec3e3c0ebb1411f7f02
Jason Whittle <jason.whittle@gmail.com>
2016-02-10 17:13:03 -0500

    Created using Spring Initializr.

---
+++ src/main/java/co/masslab/SecbugApplication.java
@@ -0,0 +1,12 @@
+package co.masslab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecbugApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecbugApplication.class, args);
+	}
+}
---
+++ src/test/java/co/masslab/SecbugApplicationTests.java
@@ -0,0 +1,18 @@
+package co.masslab;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = SecbugApplication.class)
+@WebAppConfiguration
+public class SecbugApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
